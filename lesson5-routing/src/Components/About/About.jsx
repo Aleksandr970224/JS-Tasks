d8546cd9913941b2d65fb2a090d593772820ccea
Aleksandr970224 <acceptf970224@gmail.com>
2023-05-29 19:11:49 +0300

    routing-first

---
+++ lesson5-routing/src/Components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi dignissimos expedita voluptate iure suscipit aliquid fugiat officia sequi? Aperiam molestiae qui voluptatum culpa deleniti esse debitis sunt vero pariatur! Odit voluptatum similique error.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ lesson5-routing/src/Components/Contacts/Contacts.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Contacts() {
+  return (
+    <div>
+      <h1>Contacts</h1>
+      <p>This is the contacts page.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint corporis incidunt et enim perspiciatis a cupiditate repudiandae vero aut laudantium ut tempore distinctio ad, mollitia odio ipsam quia cum exercitationem voluptatum cumque dicta pariatur expedita! Veniam itaque magnam nesciunt ut atque maxime, dolor qui dolorem.</p>
+    </div>
+  );
+}
+
+export default Contacts;
